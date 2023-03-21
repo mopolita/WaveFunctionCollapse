4fed4711dc362da8fe9e17012d9017723ea4ec32
mopolita <albanrichard48@gmail.com>
2023-03-21 22:27:35 +0100

    work on sudoku.c
    added functions :
     - init_empty
    modified functions
     - iniy_grid

---
+++ sudoku/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <err.h>
+#include "sudoku.h"
+
+int main(int argc, char* argv[])
+{
+	//TODO
+}
--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <time.h>
+#include <errno.h>
 #include <err.h>
 #include "sudoku.h"
 
-char* init_grid(size_t nb_in)
+char* init_empty()
 {
-	if (nb_in >= 81)
-	{
-		errx(1,"cannot initialise the grid : nb_in must be inferior to 81");
-	}
 	char* grid = calloc(81, sizeof(char));
 	if (grid == NULL)
-	{
 		err(1, NULL);
-	}
-	srand(time(NULL));
+	return grid;
+}
+
+char* init_grid(size_t nb_in)
+{
 	int l;
 	int c;
 	char filled[81] = {0};
-	size_t i = 0;
+	size_t i = 0;	
+	if (nb_in >= 81)
+		errx(1,"cannot initialise the grid : nb_in must be inferior to 81");
+	char* grid = calloc(81, sizeof(char));
+	if (grid == NULL)
+		err(1, NULL);
+	srand(time(NULL));
 	while(i<nb_in)
 	{
 		l = rand() % 9;
 		c = rand() % 9;
 		if (filled[l*9 + c] == 0)
 		{
-			// TODO: modify so function verifies that the number added is valid
 			filled[l*9 + c] = 1;
+			// TODO: modify so function verifies that the number added is valid
+			char valid = 0;
+			while(valid == 0)
+			{
+				valid = 1;
+			}
 			grid[l*9 + c] = rand() % 9;
 			i++;
 		}
